6b11de198fc65b49725f6b8a587937f61ecbcb5f
William B <william@barkoffusa.com>
2018-12-16 22:23:21 -0500

    particles

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+    Particles.init({
+        selector: '#particles',
+        color: '#ffffff',
+        connectParticles: 'true',
+        maxParticles: 50,
+        sizeVariations: 5
+    });
     var greetings = ["Hello", "Howdy", "Hi", "G'day", "Hey", "Hiya"];
     $("#greeting").text("👋 " + greetings[Math.floor(Math.random() * greetings.length)]);
 });
