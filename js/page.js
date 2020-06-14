f458f6bf2f54a6d9167b3ebff905e773324afce2
William Barkoff <william@barkoffusa.com>
2020-06-14 00:33:15 -0400

    Update fonts, add anayltics

--- js/page.js
+++ js/page.js
@@ -1,8 +1,14 @@
-(function () {
-    var burger = document.querySelector('.navbar-burger');
-    var menu = document.querySelector('.navbar-menu');
-    burger.addEventListener('click', function () {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
-    });
-})();
+var burger = document.querySelector('.navbar-burger');
+var menu = document.querySelector('.navbar-menu');
+burger.addEventListener('click', function () {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+});
+
+if (navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack) {
+    $(".notracking").show()
+    $(".tracking").hide()
+} else {
+    $(".tracking").show()
+    $(".notracking").hide()
+}
